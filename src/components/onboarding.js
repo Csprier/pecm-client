5d6553d8370da2336c867b0ff920b41528e5d1b8
Cam Prier <CSPrier@gmail.com>
2018-06-15 15:51:13 -0700

    edited onboarding.js

--- src/components/onboarding.js
+++ src/components/onboarding.js
@@ -8,14 +8,11 @@ export class Onboarding extends React.Component {
       <div className="onboarding-content">
         <h2>Welcome to PECM!</h2>
         <h2>(Physical Education Class Manager)</h2>
-
         <p>This is a tool teachers can use on their mobile devices to help organize daily class scheduling. </p>
-
         <p>Create an account! Upon creation, you will be automatically logged in.</p>
-
         <p>If you already have an account, simply log in!</p>
-
-        <p>You will be presented with a database full of students. From there, you have the option to filter by period via the dropdown select menu to see the students in a specified period, or to assign a period to a specific student via the student's individual dropdown select menu to add them to a period.</p>
+        <p>You will be presented with a database of students.</p>
+        <p>From there, you have the option to add or delete students. Assign them periods to be scheduled, and filter the list to see which students are in a specific period.</p>
       </div>
     )
   }
